ad5996d69466348df317c4451afae766afd2aeca
Edwin Corzo <edwin11ramirez@gmail.com>
2015-02-05 19:58:56 -0800

    made a few adjustments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -11,6 +11,8 @@ game.PlayerEntity = me.Entity.extend({
             }
         }]);
        this.body.setVelocity(5, 20);
+       //keeps track on where the player is going
+       this.facing = "right";
        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
        
        this.renderable.addAnimation("idle", [78]);
@@ -26,8 +28,10 @@ game.PlayerEntity = me.Entity.extend({
             //setVelocity and multiplying it by me.timer.tick
             //me.timer.tick makes movement look smooth
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.facing = "right";
             this.flipX(true);
         }else if(me.input.isKeyPressed("left")){
+            this.facing = "left"
             this.body.vel.x -=this.body.accel.x * me.timer.tick;
             this.flipX(false);
         }else{
@@ -49,35 +53,47 @@ game.PlayerEntity = me.Entity.extend({
                 //from the first animation, not from wherever we left off when we
                 //switched to another animation
                 this.renderable.setAnimationFrame();
-                this.renderable -=====renderableandawesome nauts and isksjjnfjfkk
+              
             }
         }
         
-        else if(this.body.vel.x !== 0) {
+        else if(this.body.vel.x !== 0 && !this.renderable.isCurrentAnimation("attack") ) {
              if(!this.renderable.isCurrentAnimation("walk")){
                  this.renderable.setCurrentAnimation("walk");
-        }
-        }else{
+            }
+        }else if(!this.renderable.isCurrentAnimation("attack")){
            this.renderable.setCurrentAnimation("idle");
         }
        
-        if(me.input.isKeyPressed("attack")){
-            if(!this.renderable.isCurrentAnimation("attack")){
-                console.log(this.renderable.isCurrentAnimation("attack"));
-                //sets the current animation to attack and once that is over
-                //goes back to the idle animation
-                this.renderable.setCurrentAnimation("attack", "idle");
-                //makes it so the next time we start this sequence we begin
-                //from the first animation, not from wherever we left off when we
-                //switched to another animation
-                this.renderable.setAnimationFrame();
-            }
-        }
-        
+        me.collision.check(this, true, this.collideHandler.bind(this), true)
         this.body.update(delta);
         
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+
+    collideHandler: function(response){
+        if(response.b.type=== 'EntityBaseEntity'){
+            var ydif = this.pos.y - response.b.pos.y;
+            var xdif = this.pos.x -response.b.posy.x;
+
+            if (ydif<-40 && xdif< 70 && xdif>-35) {
+                this.body.falling = false;
+                this.body.vel.y = -1;
+            }
+
+            else if(xdif>-35 && this.facing==='right' && (xdif<0)){
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x -1;
+            }else if(xdif<70 && this.facing==='left' && xdif>0){
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x -1;
+            }
+
+            if (this.renderable.isCurrentAnimation("attack")) {
+                    response.b.loseHealth();            
+            };
+        }
     }
 });
 
