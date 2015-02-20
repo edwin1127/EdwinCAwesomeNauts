94e65415e9e148727cd8a405efd02d8de3337e4e
Edwin Corzo <edwin11ramirez@gmail.com>
2015-02-20 12:15:27 -0800

    finsihed video 25

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -122,7 +122,7 @@ game.PlayerEntity = me.Entity.extend({
 			var xdif = this.pos.x - response.b.pos.x;
  			
  			//checks to see when character collides with enemy base
-			console.log("xdif " + xdif + "ydif " + ydif);
+			//console.log("xdif " + xdif + "ydif " + ydif);
 
 			//jumping through the top of the enemy base
 			if(ydif<-40 && xdif< 70 && xdif>-35){
@@ -138,7 +138,7 @@ game.PlayerEntity = me.Entity.extend({
 				// moves player back a bit
 				this.pos.x = this.pos.x - 1;
 				//else if statement is used if the character is facing left
-			}else if(xdif<70 && this.facing==='left' && xdif>0){
+			}else if(xdif<70 && this.facing ==='left'  && xdif>0){
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x +1;
 			}
@@ -148,6 +148,29 @@ game.PlayerEntity = me.Entity.extend({
 				this.lastHit = this.now;
 				//character dies
 				response.b.loseHealth();
+			}
+		}else if(response.b.type==='EnemyCreep'){
+			var xdif = this.pos.x - response.b.pos.x;
+			var ydif = this.pos.y - response.b.pos.y;
+
+			if (xdif>0) {
+				this.pos.x = this.pos.x + 1;
+				if (this.facing==="left") {
+					this.vel.x = 0;
+				}
+			}else{
+				this.pos.x = this.pos.x - 1;
+				if(this.facing==="right"){
+					this.vel.x = 0;
+				}
+			}
+			
+			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000
+					&& (Math.abs(ydif) <=40) && 
+					((xdif>0) && this.facing==="left")	|| ((xdif<0) &&)
+					){
+				this.lastHit = this.now;
+				response.b.loseHealth(1);
 			}
 		}
 	}
@@ -301,9 +324,15 @@ game.EnemyCreep = me.Entity.extend({
 		this.renderable.setCurrentAnimation("walk");
 	},
 
-	loseHealth: function  annssklalkske  be bded sbs
+	loseHealth: function (damage){
+		this.health = this.health - damage;	
+	},
 
 	update: function(delta){
+		if(this.health <= 0) {
+			me.game.world.removeChild(this);
+		}   
+
 		this.now = new Date().getTime();
 		//has player accelerate
 		this.body.vel.x -= this.body.accel.x * me.timer.tick;
